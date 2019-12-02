c70ef4e401832eee4db85eb0c62e896d2e28e0c4
Jorge Juan Barroso Carmona <jorge@karumi.com>
2019-12-02 02:51:44 +0100

    calcuate the fuel for sending a module

--- day1-tyranny-rocket-equation/tests/test_tyranny.py
+++ day1-tyranny-rocket-equation/tests/test_tyranny.py
@@ -1,2 +1,13 @@
-def test_my_first_try():
-    assert "first_test" == "first_test"
+from tyranny.tyranny import FuelMeasurer
+
+
+def test_should_return_the_fuel_need_to_launch_when_is_not_should_be_rounded():
+    fuel_measurer = FuelMeasurer()
+    fuel_required = fuel_measurer.measure(mass=12)
+    assert 2 == fuel_required
+
+
+def test_should_return_the_fuel_need_to_launch_when_should_be_rounded():
+    fuel_measurer = FuelMeasurer()
+    fuel_required = fuel_measurer.measure(mass=100756)
+    assert 33583 == fuel_required
---
+++ day1-tyranny-rocket-equation/tyranny/tyranny.py
@@ -0,0 +1,4 @@
+
+class FuelMeasurer:
+    def measure(self, mass: int):
+        return int(mass / 3) - 2
