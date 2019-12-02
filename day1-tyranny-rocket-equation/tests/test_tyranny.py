e370a97373a529d590c66b2aeeff70ffa405a3d7
Jorge Juan Barroso Carmona <jorge@karumi.com>
2019-12-02 03:29:26 +0100

    add the calculus of all fuels.

--- day1-tyranny-rocket-equation/tests/test_tyranny.py
+++ day1-tyranny-rocket-equation/tests/test_tyranny.py
@@ -5,22 +5,22 @@ from tyranny.tyranny import FuelMeasurer
 def test_should_return_the_fuel_need_to_launch_when_is_not_should_be_rounded():
     fuel_measurer = FuelMeasurer()
     fuel_required = fuel_measurer.measure(mass=12)
-    assert 2 == fuel_required
+    assert fuel_required == 2
 
 
 def test_should_return_the_fuel_need_to_launch_when_should_be_rounded():
     fuel_measurer = FuelMeasurer()
     fuel_required = fuel_measurer.measure(mass=100756)
-    assert 33583 == fuel_required
+    assert fuel_required == 50346
 
 
 def test_should_return_the_total_fuel_consume_to_launch_all_modules():
     fuel_measurer = FuelMeasurer()
     total_fuel_required = fuel_measurer.measure_all(masses=[12, 14])
-    assert 4 == total_fuel_required
+    assert total_fuel_required == 4
 
 
 def test_should_return_the_total_fuel_consume_to_the_input_values():
     fuel_measurer = FuelMeasurer()
     total_fuel_required = fuel_measurer.measure_all(masses=get_masses_mock_values())
-    assert 3412531 == total_fuel_required
+    assert total_fuel_required == 5115927
